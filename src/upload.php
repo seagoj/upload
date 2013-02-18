c3a62ff690ed1579a4f19b261aee7272f8b357aa
Jeremy Seago <seagoj@gmail.com>
2013-02-18 12:37:44 -0600

    Adds check for POST variables to upload

--- src/upload.php
+++ src/upload.php
@@ -24,6 +24,8 @@ function upload()
         echo "$extension is an invalid file type.";
     }
 }
+
+var_dump($_POST);
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
