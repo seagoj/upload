956da3895f8646862eb72e02646ae41578b4f1ca
Jeremy Seago <seagoj@gmail.com>
2013-02-18 12:20:45 -0600

    updated layout of index

--- src/index.php
+++ src/index.php
@@ -1,12 +1,191 @@
-<html>
-<body>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en-US">
+<head profile="http://gmpg.org/xfn/11">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>  Tax Planning and Compliance | Burwitz and Sostak</title>
+<link rel="canonical" href="http://www.bas-cpa.com/?page_id=223" />
+<link rel="stylesheet" href="http://www.bas-cpa.com/BAS-CPA/wp-content/themes/Minimal/style.css" type="text/css" media="screen" />
+<link rel="alternate" type="application/rss+xml" title="Burwitz and Sostak RSS Feed" href="http://www.bas-cpa.com/?feed=rss2" />
+<link rel="alternate" type="application/atom+xml" title="Burwitz and Sostak Atom Feed" href="http://www.bas-cpa.com/?feed=atom" />
+<link rel="pingback" href="http://www.bas-cpa.com/BAS-CPA/xmlrpc.php" />
 
-<form action="upload.php" method="post"
-enctype="multipart/form-data">
+<!--[if lt IE 7]>
+    <link rel="stylesheet" type="text/css" href="http://www.bas-cpa.com/BAS-CPA/wp-content/themes/Minimal/css/ie6style.css" />
+    <script type="text/javascript" src="http://www.bas-cpa.com/BAS-CPA/wp-content/themes/Minimal/js/DD_belatedPNG_0.0.8a-min.js"></script>
+    <script type="text/javascript">DD_belatedPNG.fix('img#logo, #header #search-form, #slogan, a#left_arrow, a#right_arrow, div.slide img.thumb, div#controllers a, a.readmore, a.readmore span, #services .one-third, #services .one-third.first img.icon, #services img.icon, div.sidebar-block .widget ul li');</script>
+<![endif]-->
+<!--[if IE 7]>
+    <link rel="stylesheet" type="text/css" href="http://www.bas-cpa.com/BAS-CPA/wp-content/themes/Minimal/css/ie7style.css" />
+<![endif]-->
+
+<script type="text/javascript">
+    document.documentElement.className = 'js';
+</script>
+
+<link rel="alternate" type="application/rss+xml" title="Burwitz and Sostak &raquo; Feed" href="http://www.bas-cpa.com/?feed=rss2" />
+<link rel="alternate" type="application/rss+xml" title="Burwitz and Sostak &raquo; Comments Feed" href="http://www.bas-cpa.com/?feed=comments-rss2" />
+<link rel="alternate" type="application/rss+xml" title="Burwitz and Sostak &raquo; Tax Planning and Compliance Comments Feed" href="http://www.bas-cpa.com/?feed=rss2&amp;page_id=223" />
+        <link rel="stylesheet" href="http://bas-cpa.com/BAS-CPA/wp-content/themes/Minimal-child/style.css" type="text/css" media="screen" />
+    <meta content="Minimal v.3.1" name="generator"/><link media="screen" type="text/css" href="http://www.bas-cpa.com/BAS-CPA/wp-content/themes/Minimal/epanel/page_templates/js/fancybox/jquery.fancybox-1.3.4.css" rel="stylesheet" />
+<link media="screen" type="text/css" href="http://www.bas-cpa.com/BAS-CPA/wp-content/themes/Minimal/epanel/page_templates/page_templates.css" rel="stylesheet" /><link rel='stylesheet' id='et-shortcodes-css-css'  href='http://www.bas-cpa.com/BAS-CPA/wp-content/themes/Minimal/epanel/shortcodes/shortcodes.css?ver=1.7' type='text/css' media='all' />
+<script type='text/javascript' src='http://www.bas-cpa.com/BAS-CPA/wp-includes/js/l10n.js?ver=20101110'></script>
+<script type='text/javascript' src='http://www.bas-cpa.com/BAS-CPA/wp-includes/js/comment-reply.js?ver=20090102'></script>
+<script type='text/javascript' src='http://www.bas-cpa.com/BAS-CPA/wp-includes/js/jquery/jquery.js?ver=1.6.1'></script>
+<script type='text/javascript' src='http://www.bas-cpa.com/BAS-CPA/wp-content/themes/Minimal/epanel/shortcodes/js/et_shortcodes_frontend.js?ver=1.7'></script>
+<link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://www.bas-cpa.com/BAS-CPA/xmlrpc.php?rsd" />
+<link rel="wlwmanifest" type="application/wlwmanifest+xml" href="http://www.bas-cpa.com/BAS-CPA/wp-includes/wlwmanifest.xml" /> 
+<link rel='index' title='Burwitz and Sostak' href='http://www.bas-cpa.com' />
+<link rel='prev' title='Management Advisory Services' href='http://www.bas-cpa.com/?page_id=230' />
+<link rel='next' title='QuickBooks Consulting' href='http://www.bas-cpa.com/?page_id=243' />
+<meta name="generator" content="WordPress 3.2.1" />
+<link rel='canonical' href='http://www.bas-cpa.com/?page_id=223' />
+    <style type="text/css">
+        #et_pt_portfolio_gallery { margin-left: -11px; }
+        .et_pt_portfolio_item { margin-left: 23px; }
+        .et_portfolio_small { margin-left: -39px !important; }
+        .et_portfolio_small .et_pt_portfolio_item { margin-left: 35px !important; }
+        .et_portfolio_large { margin-left: -20px !important; }
+        .et_portfolio_large .et_pt_portfolio_item { margin-left: 14px !important; }
+    </style>
+
+</head>
+<body>
+    <div id="page-wrap">
+            
+        <div id="header">
+            <!-- Start Logo -->
+                <a href="http://www.bas-cpa.com">                   <img src="http://www.bas-cpa.com/BAS-CPA/wp-content/uploads/2011/08/logo21.png" alt="Logo" id="logo"/></a>
+                
+                <p id="slogan"></p>
+                    
+            <!-- End Logo -->
+                    
+            <!-- Start Searchbox -->
+                <div id="search-form">
+                    <form method="get" id="searchform1" action="http://www.bas-cpa.com/">
+                        <input type="text" value="search this site..." name="s" id="searchinput" />
+                    </form>
+                </div>
+                                
+            <!-- End Searchbox -->
+                
+                <div class="clear"></div>
+                
+                <ul id="menu-header" class="superfish nav clearfix"><li id="menu-item-39" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-home menu-item-39"><a href="http://www.bas-cpa.com">Home</a></li>
+<li id="menu-item-225" class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-223 current_page_item menu-item-225"><a href="http://www.bas-cpa.com/?page_id=223">Tax Planning and Compliance</a></li>
+<li id="menu-item-232" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-232"><a href="http://www.bas-cpa.com/?page_id=230">Management Advisory Services</a></li>
+<li id="menu-item-141" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-141"><a href="http://www.bas-cpa.com/?page_id=29">Outsourced Accounting</a></li>
+<li id="menu-item-245" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-245"><a href="http://www.bas-cpa.com/?page_id=243">QuickBooks Consulting</a></li>
+<li id="menu-item-44" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-44"><a href="http://www.bas-cpa.com/?page_id=35">Contact Us</a></li>
+</ul>                                   
+        </div> <!-- end #header -->
+
+        <div id="breadcrumbs">
+        
+                    <a href="http://www.bas-cpa.com">Home</a> &raquo;
+                
+                                      Tax Planning and Compliance                   
+</div> <!-- end #breadcrumbs -->        
+        <div id="content" class="clearfix pagefull_width">
+            <div id="content-area">         
+                <div class="entry clearfix">
+                    <h1 class="title">File Transfer</h1>
+                    <h3>Please submit your file via the form below:</h3>
+                    <h5><form action="upload.php" method="post" enctype="multipart/form-data">
 <label for="file">Filename:</label>
 <input type="file" name="file" id="file"><br>
 <input type="submit" name="submit" value="Submit">
-</form>
+</form></h5>
+<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span id="more-223"></span><br />
+<a href="http://www.bas-cpa.com/BAS-CPA/wp-content/uploads/2011/08/construction.png"><img src="http://www.bas-cpa.com/BAS-CPA/wp-content/uploads/2011/08/construction.png" alt="" title="construction" width="550" height="150" class="alignleft size-full wp-image-212" /></a></p>
+                                                                        </div> <!-- end .entry -->
+                            
+                                    
+            </div> <!-- end #content-area -->   
 
+        </div> <!-- end #content --> 
+        
+         <div id="footer" >
+        <div id="footer-content">
+            
+                                
+                <ul id="menu-footer" class="bottom-menu"></ul>          
+            <p id="copyright">Copyright 2011 <a href="http://www.froeschnercreative.com" title="Froeschner Creative">Froeschner Creative</a> &copy; All rights reserved</p>
+        </div> <!-- end #footer-content -->
+    </div> <!-- end #footer -->
+</div> <!-- end #page-wrap -->
+     
+                
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.1/jquery-ui.min.js"></script>
+    <script type="text/javascript" src="http://www.bas-cpa.com/BAS-CPA/wp-content/themes/Minimal/js/jquery.cycle.all.min.js"></script> 
+    <script type="text/javascript" src="http://www.bas-cpa.com/BAS-CPA/wp-content/themes/Minimal/js/jquery.easing.1.3.js"></script> 
+    <script type="text/javascript" src="http://www.bas-cpa.com/BAS-CPA/wp-content/themes/Minimal/js/superfish.js"></script> 
+    <script type="text/javascript">
+    //<![CDATA[
+        jQuery.noConflict();
+    
+        jQuery('ul.superfish').superfish({ 
+            delay:       200,                            // one second delay on mouseout 
+            animation:   {opacity:'show',height:'show'},  // fade-in and slide-down animation 
+            speed:       'fast',                          // faster animation speed 
+            autoArrows:  true,                           // disable generation of arrow mark-up 
+            dropShadows: false                            // disable drop shadows 
+        }).find("> li > ul > li:last-child, > li > ul > li > ul > li:last-child, > li > ul > li > ul > li > ul > li:last-child").addClass("last-nav-element");
+        
+        jQuery(".js #featured, .js div#tabbed").css("display","block");
+
+        jQuery("ul.nav > li > ul").prev("a").attr("href","#");      
+        var $featured_content = jQuery('#featured_content'),
+            $tabbed_area = jQuery('div#tabbed'),
+            $controllers = jQuery('div#controllers'),
+            $comments = jQuery('ol.commentlist');
+        
+        et_search_bar();
+        
+        if ($featured_content.length) {
+            $featured_content.cycle({
+                                    timeout: 5000               ,
+                speed: 300,
+                cleartypeNoBg: true,
+                prev:   'a#left_arrow', 
+                next:   'a#right_arrow',
+                pager:  'div#controllers',
+                fx: 'fade'
+            });
+            
+            var controllersWidth = $controllers.width(),
+                controllersLeft = Math.round((960 - controllersWidth) / 2);
+            if (controllersWidth < 960) $controllers.css('padding-left',controllersLeft);
+        };
+        
+        if ($tabbed_area.length) {
+            $tabbed_area.tabs({ fx: { opacity: 'toggle' } });
+        };
+        
+        if ($comments.length) {
+            $comments.find(">li").after('<span class="bottom_bg"></span>');
+        };
+        
+        
+        <!---- Search Bar Improvements ---->
+        function et_search_bar(){
+            var $searchform = jQuery('#header div#search-form'),
+                $searchinput = $searchform.find("input#searchinput"),
+                searchvalue = $searchinput.val();
+                
+            $searchinput.focus(function(){
+                if (jQuery(this).val() === searchvalue) jQuery(this).val("");
+            }).blur(function(){
+                if (jQuery(this).val() === "") jQuery(this).val(searchvalue);
+            });
+        };
+        
+    //]]>   
+    </script>
+    <script type='text/javascript' src='http://www.bas-cpa.com/BAS-CPA/wp-content/themes/Minimal/epanel/page_templates/js/fancybox/jquery.easing-1.3.pack.js?ver=1.3.4'></script>
+<script type='text/javascript' src='http://www.bas-cpa.com/BAS-CPA/wp-content/themes/Minimal/epanel/page_templates/js/fancybox/jquery.fancybox-1.3.4.pack.js?ver=1.3.4'></script>
+<script type='text/javascript' src='http://www.bas-cpa.com/BAS-CPA/wp-content/themes/Minimal/epanel/page_templates/js/et-ptemplates-frontend.js?ver=1.1'></script>
+    
 </body>
-</html>
+</html> 
+    
--- src/upload.php
+++ src/upload.php
@@ -8,7 +8,7 @@ if (in_array($extension, $allowedExts)) {
         echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
     } else {
         echo "Upload: " . $_FILES["file"]["name"] . "<br>";
-        echo "Type: " . $_FILES["file"]["type"] . "<br>";
+        // echo "Type: " . $_FILES["file"]["type"] . "<br>";
         echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
 
         if (file_exists($savePath . $_FILES["file"]["name"])) {
