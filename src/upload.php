0f8860d7ba0638de144151615ccd81e676345183
Jeremy Seago <seagoj@gmail.com>
2013-02-18 11:59:59 -0600

    adjusts upload path

--- src/upload.php
+++ src/upload.php
@@ -16,11 +16,11 @@ if (in_array($extension, $allowedExts)) {
         echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
         echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>";
 
-        if (file_exists("upload/" . $_FILES["file"]["name"])) {
+        if (file_exists("../storage/" . $_FILES["file"]["name"])) {
             echo $_FILES["file"]["name"] . " already exists. ";
         } else {
-            move_uploaded_file($_FILES["file"]["tmp_name"], "upload/" . $_FILES["file"]["name"]);
-            echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
+            move_uploaded_file($_FILES["file"]["tmp_name"], "../storage/" . $_FILES["file"]["name"]);
+            echo "Stored in: " . "storage/" . $_FILES["file"]["name"];
         }
     }
 } else {
