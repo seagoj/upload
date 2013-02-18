c7976f8e951ea8ce4e5e1029a68f2751581fc8fc
Jeremy Seago <seagoj@gmail.com>
2013-02-18 12:10:53 -0600

    removed display of Temp location

--- src/upload.php
+++ src/upload.php
@@ -10,14 +10,14 @@ if (in_array($extension, $allowedExts)) {
         echo "Upload: " . $_FILES["file"]["name"] . "<br>";
         echo "Type: " . $_FILES["file"]["type"] . "<br>";
         echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
-        echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>";
+        // echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>";
 
         if (file_exists($savePath . $_FILES["file"]["name"])) {
             echo $_FILES["file"]["name"] . " already exists. ";
         } else {
             move_uploaded_file($_FILES["file"]["tmp_name"], $savePath . $_FILES["file"]["name"]);
             // echo "Stored in: " . "storage/" . $_FILES["file"]["name"];\
-            echo $_FILES["file"]["name"]." has been successfully uplaoded";
+            echo $_FILES["file"]["name"]." has been successfully uploaded";
         }
     }
 } else {
